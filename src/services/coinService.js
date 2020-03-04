9d5c3db0588f468da09e6aa51c1d4e75531167a6
ArturWk4 <elkin.as@mail.ru>
2020-03-04 14:15:01 +0300

    rename method getAllCoinsTitle to fetchAllCoinsTitle

--- src/services/coinService.js
+++ src/services/coinService.js
@@ -7,10 +7,10 @@ const CoinGeckoClient = new CoinGecko();
 const listCoins = async (page, order) =>
   CoinGeckoClient.coins.all({ per_page, page, order });
 
-const getAllCoinsTitle = () => CoinGeckoClient.coins.list();
+const fetchAllCoinsTitle = () => CoinGeckoClient.coins.list();
 
 const fetchCoinDetails = name => CoinGeckoClient.coins.fetch(name);
 
 const getMarketChart = id => CoinGeckoClient.coins.fetchMarketChart(id);
 
-export { listCoins, getAllCoinsTitle, fetchCoinDetails, getMarketChart };
+export { listCoins, fetchAllCoinsTitle, fetchCoinDetails, getMarketChart };
